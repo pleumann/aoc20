6c9cbc9d51b7fecd66408550061c0a1b2949e662
joerg <joerg.pleumann@gmail.com>
2021-05-01 19:03:11 +0200

    Adding a comment for day 8

--- src/day08/Puzzle2.java
+++ src/day08/Puzzle2.java
@@ -13,6 +13,16 @@ public class Puzzle2 extends Puzzle1 {
          * The lazy way to solve the puzzle. Try to change a "jmp" or "nop"
          * instruction in the program into "nop" or "jmp" (respectively) and
          * run the program. Wash, rinse, repeat until things terminate.
+         *
+         * Possible optimizations, both assuming part 1 has been run first:
+         *
+         * 1) An instruction that has not been executed in part 1 cannot make
+         *    the difference between infinite loop and proper termination,
+         *    hence we don't need to try these.
+         *
+         * 2) If we keep track of accumulator values at each instruction and a
+         *    singly-linked that tells us from where an instruction was reached
+         *    it is possible to backtrack, resulting in a very fast solution.
          */
         for (int i = 0; i < p.input.length; i++) {
             if (p.input[i].startsWith("jmp")) {
